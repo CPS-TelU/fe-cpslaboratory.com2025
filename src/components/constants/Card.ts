b4f4e73d1d2a82b5e36915ed75e1a4802d4d3c2e
NRacoo <ariaaurar@gmail.com>
2025-08-28 19:03:35 +0700

    first commit:add Hero&WhatWeDo

---
+++ src/components/constants/Card.ts
@@ -0,0 +1,28 @@
+
+
+export const WhatWeDoContent = [
+    {
+        title:"Things #1",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        imageUrl:"/images/Card1.png",
+        imageAlt:"image"
+    },
+    {
+        title:"Things #2",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        imageUrl:"/images/Card1.png",
+        imageAlt:"image"
+    },
+    {
+        title:"Things #3",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        imageUrl:"/images/Card1.png",
+        imageAlt:"image"
+    },
+    {
+        title:"Things #4",
+        description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        imageUrl:"/images/Card1.png",
+        imageAlt:"image"
+    },
+]

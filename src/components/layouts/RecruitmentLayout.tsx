a59bf6d8ddabca32f7ce0e5740e477cb6ca34e6e
asepjamaludinn <asepjamaludinn24@gmail.com>
2025-12-12 23:16:07 +0700

    refac: design recruitment

--- src/components/layouts/RecruitmentLayout.tsx
+++ src/components/layouts/RecruitmentLayout.tsx
@@ -1,14 +1,24 @@
 import SectionContainer from "@/utils/SectionsContainer";
 import RecruitmentPage from "../contents/RecruitmentPage";
 import RecruitmentAbout from "../contents/RecruitmentAbout";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-poppins",
+});
 
 export default function RecruitmentLayout() {
-    return(
-        <div className="w-full">
-            <SectionContainer>
-                <RecruitmentPage/>
-                <RecruitmentAbout/>
-            </SectionContainer>
+  return (
+    <div className={`w-full ${poppins.className}`}>
+      <SectionContainer>
+        <div className="flex flex-col gap-12 pb-20">
+          {" "}
+          <RecruitmentPage />
+          <RecruitmentAbout />
         </div>
-    )
+      </SectionContainer>
+    </div>
+  );
 }
